bcd4374d2e902c27884ebd0aca02387143cc5c09
sachour <sachour@mit.edu>
2018-11-16 14:50:17 -0500

    added basic harness for measuring dac

--- grendel.py
+++ grendel.py
@@ -1,157 +1,109 @@
+import argparse
+import os
+import sys
 
-# grendel generates scripts to exercise a component.
-import json
-from enum import Enum
-import scipy,scipy.signal
-import numpy as np
-import matplotlib.pyplot as plt
+sys.path.insert(0,os.path.abspath("lab_bench"))
 
-class EmpiricalData:
-    class TimeSeries:
-        def __init__(self,time,value):
-            self.times = time;
-            self.values = value;
+import lab_bench.lib.state as lab_state
+import lab_bench.lib.command_handler as lab_handler
 
-        def shift_left(self,amount):
-            self.times = list(filter(
-                lambda t: t >= 0,
-                map(lambda t: t-amount,
-                    self.times)))
-            offset = len(self.values) - len(self.times)
-            self.values = self.values[offset:]
-
-        def truncate_after(self,max_time):
-            self.times = list(filter(
-                lambda t: t <= max_time, self.times))
-            offset = len(self.values) - len(self.times)
-            self.values = self.values[:-offset]
+def boilerplate_osc(prog,sim_time,ref_fun,dacs):
+    def q(stmt):
+        prog.append(stmt)
 
 
-        def resample(self,npts):
-            times = self.times
+    q('set_ref_func %s' % ref_fun)
+    q('set_sim_time %f' % sim_time)
+    q('get_num_samples')
+    q('get_time_between_samples')
+    q('use_osc')
+    for dac in dacs:
+        q('use_due_dac %d' % dac)
 
-            rsvals,rstimes = scipy.signal.resample(
-                self.values,npts,
-                t=times)
-            return rstimes,rsvals
+    q('compute_offsets')
 
-        def difference(self,other,npts=100000):
-            t1,v1 = self.resample(npts)
-            t2,v2 = other.resample(npts)
-            vsub = list(map(lambda args: args[0]-args[1],
-                            zip(v1,v2)))
-            return EmpiricalData.TimeSeries(t1,vsub)
+def test_dac_on_input(dac_id,input_fn,num_trials=5):
+    prog = []
+    def q(stmt):
+        prog.append(stmt)
 
-        def plot(self):
-            plt.plot(self.times,self.values)
+    boilerplate_osc(prog,20.0,"inp0+1.5",[dac_id])
+    q('set_due_dac_values %d %s' % (dac_id,input_fn))
+    q('run')
+    for trial in range(0,num_trials):
+        q('get_osc_values direct data%d.json' % trial)
 
-    def __init__(self):
-        self._inputs = {}
-        self._output = None
-        self._reference = None
+    q('reset')
+    return prog
 
-    @property
-    def output(self):
-        return self._output
-
-    @property
-    def reference(self):
-        return self._reference
-
-    def input(self,index):
-        return self._inputs[index]
-
-    def set_output(self,time,value):
-        self._output = EmpiricalData.TimeSeries(time,value)
-
-    def set_reference(self,time,value):
-        self._reference = EmpiricalData.TimeSeries(time,value)
-
-    def set_input(self,index,time,value):
-        self._inputs[index] = EmpiricalData.TimeSeries(time,value)
+def exec_prog(state,blockid,index,prog):
+    outdir = "outputs/grendel/%s/%s/" % (blockid,index)
 
 
-    def plot(self):
-        self._output.plot()
-        self._reference.plot()
-        plt.savefig('processed.png')
-        plt.clf()
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-    def align(self,npts=1000):
-        def mse(x,y,offset):
-            error = sum(map(lambda val: val**2, x[:offset]))
-            if offset == 0:
-                error += sum(map(lambda args: (args[0]-args[1])**2,
-                                 zip(x,y)))
+    scriptfile = "%s/prog.grendel" % outdir
+    with open(scriptfile,'w') as fh:
+        fh.write("\n".join(prog))
 
-            else:
-                error += sum(map(lambda args: (args[0]-args[1])**2,
-                                 zip(x[offset:],y[:-offset])))
-            return error
+    lab_handler.main_script(state,scriptfile)
 
-        npts = 1000
-        time1,samp1= self._output.resample(npts)
-        time2,samp2= self._reference.resample(npts)
-        min_error = None
-        best_offset = None
-        for offset in range(0,npts):
-            error = mse(samp1,samp2,offset)
-            if min_error is None or error < min_error:
-                min_error = error
-                best_offset = offset
+    for dirname, subdirlist, filelist in os.walk(outdir):
+        for fname in filelist:
+            if fname.endswith('json'):
+                print(fname)
 
-        delta_x= np.mean(np.diff(time1))
-        shift = best_offset*delta_x
-        # save the phase delay from the empirical data
-        self.phase_delay = shift
-        # clip the output signal to only keep the part over the
-        # reference signal
-        self.output.shift_left(shift)
-        max_time = max(self.reference.times)
-        self.output.truncate_after(max_time)
 
-class EmpiricalDatumType(Enum):
-    INPUT = "input"
-    REFERENCE = "reference"
-    OUTPUT = "output"
 
-def read(filename):
-    data = None
-    with open(filename,'r') as fh:
-        data = json.loads(fh.read())
-    
-    assert(not data is None)
-    empd = EmpiricalData()
-    for key,datum in data.items():
-        typ = EmpiricalDatumType(datum['type'])
-        if typ == EmpiricalDatumType.INPUT:
-            empd.set_input(datum['index'],datum['time'],datum['value'])
-        elif typ == EmpiricalDatumType.OUTPUT:
-            empd.set_output(datum['time'],datum['value'])
-        elif typ == EmpiricalDatumType.REFERENCE:
-            empd.set_reference(datum['time'],datum['value'])
+def due_dac_runner(state,dac_id):
+    inputs = [
+        "0", "1",
+        "1.0*math.sin(1.0*t)",
+        "1.0*math.sin(10.0*t)",
+        "1.0*math.sin(100.0*t)",
+        "0.2*math.sin(1.0*t)"
+    ]
+    for index,input_sig in enumerate(inputs):
+        exec_prog(state,"dac[0]",index,test_dac_on_input(dac_id,input_sig))
 
-    return empd
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inputs", type=int,help="number of inputs.")
-    parser.add_argument("--block",type=str,help="block to exercise")
-    parser.add_argument("--chip", type=int,help="chip number.")
-    parser.add_argument("--tile", type=int,help="tile number.")
-    parser.add_argument("--slice", type=int,help="slice number.")
-    parser.add_argument("--index", type=int,help="index number.")
+    parser.add_argument("--ip", type=str, help="ip address of oscilloscope.")
+    parser.add_argument("--port", type=int, default=5024, help="port number of oscilloscope.")
+    parser.add_argument("--native", action='store_true',help="use native mode for arduino DUE.")
+
+    subparsers = parser.add_subparsers(dest='subparser_name',
+                                       help='blocks to exercise.')
+
+    due_dac = subparsers.add_parser('due_dac', help='profile the Arduino Due DAC')
+    due_dac.add_argument('--dac-id', type=int,default=0, help='dac id to evaluate.')
+
+    volt_div = subparsers.add_parser('vdiv', help='generate the voltage divider')
+    volt_div.add_argument('--dac-id', type=int,default=0, help='dac id to evaluate.')
+
+
+    hdac_v2= subparsers.add_parser('analog_chip', help='exercise a block in the chip.')
+    hdac_v2.add_argument("--block",type=str,help="block to exercise")
+    hdac_v2.add_argument("--chip", type=int,help="chip number.")
+    hdac_v2.add_argument("--tile", type=int,help="tile number.")
+    hdac_v2.add_argument("--slice", type=int,help="slice number.")
+    hdac_v2.add_argument("--index", type=int,help="index number.")
 
     args = parser.parse_args()
 
+    state = lab_state.State(args.ip,args.port,
+                  ard_native=args.native,
+                  validate=True)
 
-empdata=read('lab_bench/data.json')
-print("=== Align Signals ===")
-empdata.align()
-empdata.plot()
 
-print("=== Generate Noise ===")
-noise = empdata.output.difference(empdata.reference)
-noise.plot()
-plt.savefig('noise.png')
-plt.clf()
+    if args.subparser_name == "due_dac":
+        due_dac_runner(state,args.dac_id)
+    else:
+        raise Exception("unhandled: %s" % args.subparser_name)
+
+
+main()
+
---
+++ lab_bench/analysis/waveform.py
@@ -0,0 +1,193 @@
+
+# grendel generates scripts to exercise a component.
+import json
+from enum import Enum
+import scipy,scipy.signal
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+import math
+
+class FrequencyData:
+
+    def __init__(self,freqs,phasors,cutoff=1e-3):
+        assert(len(freqs) == len(phasors))
+        self._freqs = freqs
+        self._phasors = phasors
+        selector = [np.absolute(x) > cutoff for x in phasors]
+        self._freqs = list(itertools.compress(freqs, selector))
+        self._phasors = list(itertools.compress(phasors,selector))
+
+    def plot(self,basename):
+        def stem_plot(filename,axname,x,y,do_log=False):
+            fig, ax = plt.subplots()
+            ax.stem(x,y,markerfmt=' ')
+            if do_log:
+                ax.set_yscale('log')
+            ax.set_xlabel('Frequency (Hz)')
+            ax.set_ylabel('%s (log(dB))' % axname)
+            fig.savefig(filename)
+
+        stem_plot("%s_ampl.png" % basename,"Amplitude",
+                  self._freqs,np.real(self._phasors))
+        stem_plot("%s_phase.png" % basename,"Phase",
+                  self._freqs,np.imag(self._phasors))
+
+class EmpiricalData:
+    class TimeSeries:
+        def __init__(self,time,value):
+            self.times = time;
+            self.values = value;
+
+        def shift_left(self,amount):
+            self.times = list(filter(
+                lambda t: t >= 0,
+                map(lambda t: t-amount,
+                    self.times)))
+            offset = len(self.values) - len(self.times)
+            self.values = self.values[offset:]
+
+        def truncate_after(self,max_time):
+            self.times = list(filter(
+                lambda t: t <= max_time, self.times))
+            offset = len(self.values) - len(self.times)
+            self.values = self.values[:-offset]
+
+
+        def resample(self,npts):
+            times = self.times
+            rsvals,rstimes = scipy.signal.resample(
+                self.values,npts,
+                t=times)
+            return rstimes,rsvals
+
+        def difference(self,other,npts=100000):
+            t1,v1 = self.resample(npts)
+            t2,v2 = other.resample(npts)
+            vsub = list(map(lambda args: args[0]-args[1],
+                            zip(v1,v2)))
+            return EmpiricalData.TimeSeries(t1,vsub)
+
+        def plot_series(self):
+            plt.plot(self.times,self.values)
+
+        def fft(self,basename=None,npts=100000):
+            t,x = self.resample(npts)
+            timestep = np.mean(np.diff(t))
+            N = len(t)
+            vf = 2.0/N*scipy.fftpack.fft(x)
+            tf = np.fft.fftfreq(N, d=timestep)
+            print("#phasors: %d" % len(vf))
+            print("#freqs: %d" % len(tf))
+            return FrequencyData(tf,vf)
+
+
+    def __init__(self):
+        self._inputs = {}
+        self._output = None
+        self._reference = None
+
+    @property
+    def output(self):
+        return self._output
+
+    @property
+    def reference(self):
+        return self._reference
+
+    def input(self,index):
+        return self._inputs[index]
+
+    def set_output(self,time,value):
+        self._output = EmpiricalData.TimeSeries(time,value)
+
+    def set_reference(self,time,value):
+        self._reference = EmpiricalData.TimeSeries(time,value)
+
+    def set_input(self,index,time,value):
+        self._inputs[index] = EmpiricalData.TimeSeries(time,value)
+
+
+    def plot(self,figname):
+        self._output.plot_series()
+        self._reference.plot_series()
+        plt.savefig(figname)
+        plt.clf()
+
+    def align(self,npts=1000):
+        def mse(x,y,offset):
+            error = sum(map(lambda val: val**2, x[:offset]))
+            if offset == 0:
+                error += sum(map(lambda args: (args[0]-args[1])**2,
+                                 zip(x,y)))
+
+            else:
+                error += sum(map(lambda args: (args[0]-args[1])**2,
+                                 zip(x[offset:],y[:-offset])))
+            return error
+
+        npts = 1000
+        time1,samp1= self._output.resample(npts)
+        time2,samp2= self._reference.resample(npts)
+        min_error = None
+        best_offset = None
+        for offset in range(0,npts):
+            error = mse(samp1,samp2,offset)
+            if min_error is None or error < min_error:
+                min_error = error
+                best_offset = offset
+
+        delta_x= np.mean(np.diff(time1))
+        shift = best_offset*delta_x
+        # save the phase delay from the empirical data
+        self.phase_delay = shift
+        # clip the output signal to only keep the part over the
+        # reference signal
+        self.output.shift_left(shift)
+        max_time = max(self.reference.times)
+        self.output.truncate_after(max_time)
+
+class EmpiricalDatumType(Enum):
+    INPUT = "input"
+    REFERENCE = "reference"
+    OUTPUT = "output"
+
+def read(filename):
+    data = None
+    with open(filename,'r') as fh:
+        data = json.loads(fh.read())
+    
+    assert(not data is None)
+    empd = EmpiricalData()
+    for key,datum in data.items():
+        typ = EmpiricalDatumType(datum['type'])
+        if typ == EmpiricalDatumType.INPUT:
+            empd.set_input(datum['index'],datum['time'],datum['value'])
+        elif typ == EmpiricalDatumType.OUTPUT:
+            empd.set_output(datum['time'],datum['value'])
+        elif typ == EmpiricalDatumType.REFERENCE:
+            empd.set_reference(datum['time'],datum['value'])
+
+    return empd
+
+def example():
+    print("=== Read Data ===")
+    empdata=read('lab_bench/data.json')
+    print("=== Align Signals ===")
+    empdata.align()
+    empdata.plot('aligned.png')
+
+    sig_f = empdata.reference.fft()
+    sig_f.plot("reference")
+
+    out_f = empdata.output.fft()
+    out_f.plot("output")
+
+    print("=== Generate Noise ===")
+    noise = empdata.output.difference(empdata.reference)
+    plt.clf()
+    noise.plot_series()
+    plt.savefig('noise.png')
+    print("=== Noise Frequencies ===")
+    noise_f = noise.fft()
+    noise_f.plot("noise")
