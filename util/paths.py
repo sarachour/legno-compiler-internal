ab732ec8791168d3c6dd80bd4c53cd43bdcf2cdd
sachour <sachour@mit.edu>
2018-12-21 16:27:38 -0500

    added paths

---
+++ util/paths.py
@@ -0,0 +1,38 @@
+
+import os
+from enum import Enum
+
+class PathHandler:
+    def __init__(self,name,bmark):
+        self.set_root_dir(name,bmark)
+        for path in [
+            self.ROOT_DIR,
+            self.BMARK_DIR,
+            self.ABS_CIRC_DIR,
+            self.ABS_GRAPH_DIR
+        ]:
+          if not os.path.exists(path):
+            os.makedirs(path)
+
+        self._name = name
+        self._bmark = bmark
+
+    def set_root_dir(self,name,bmark):
+        self.ROOT_DIR = "outputs/legno/%s" % name
+        self.BMARK_DIR = self.ROOT_DIR + ("/%s" % bmark)
+        self.ABS_CIRC_DIR = self.BMARK_DIR + "/abs-circ"
+        self.ABS_GRAPH_DIR = self.BMARK_DIR + "/abs-graph"
+
+    def abs_graph_file(self,indices):
+        index_str = "_".join(map(lambda ind : str(ind),indices))
+        return self.ABS_GRAPH_DIR+ "/%s_%s.dot" % \
+          (self._bmark,index_str)
+
+
+    def abs_circ_file(self,indices):
+        index_str = "_".join(map(lambda ind : str(ind),indices))
+        return self.ABS_CIRC_DIR+ "/%s_%s.circ" % \
+          (self._bmark,index_str)
+
+    def has_file(self,filepath):
+        return os.path.exists(filepath)
