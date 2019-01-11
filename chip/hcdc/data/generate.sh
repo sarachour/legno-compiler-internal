70776761413d6c7dfe7ec77e31352eb4e491d697
sachour <sachour@mit.edu>
2019-01-11 11:31:51 -0500

    marked scf for integ

--- chip/hcdc/data/generate.sh
+++ chip/hcdc/data/generate.sh
@@ -7,4 +7,4 @@ python3 genbb.py mult10x.csv in0 breaks.txt mult10x.bb
 python3 genbb.py mult1x.csv in0 breaks.txt mult1x.bb
 python3 genbb.py global_xbar.csv in breaks.txt global_xbar.bb
 python3 genbb.py tile_xbar.csv in breaks.txt tile_xbar.bb
-python3 genbb.py integ.csv in breaks.txt integ.bb
+python3 genbb.py integ1x.csv in breaks.txt integ1x.bb
