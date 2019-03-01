874959c9279587eced75e61b206ebf7a020d7cde
sachour <sachour@mit.edu>
2019-03-01 17:36:55 -0500

    fixed

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@ rm -rf outputs/legno/default/$BMARK*
 python3 legno.py $BMARK arco --xforms 1 --abs-circuits 2 --conc-circuits 1
 python3 legno.py $BMARK jaunt --scale-circuits 2
 python3 legno.py $BMARK skelter
-python3 legno.py $BMARK jaunt --physical
-python3 legno.py $BMARK skelter 
 python3 legno.py $BMARK srcgen default
