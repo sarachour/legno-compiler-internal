2ce877722dc1597066ead6f479a794d7f572ad75
sachour <sachour@mit.edu>
2019-04-14 12:48:43 -0400

    added

--- lab_bench/arduino/microphone_bench/Main.cpp
+++ lab_bench/arduino/microphone_bench/Main.cpp
@@ -4,13 +4,31 @@ Example Sound Level Sketch for the
 Adafruit Microphone Amplifier
 ****************************************/
 
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+long duration;
+
 void setup()
 {
    Serial.begin(9600);
    analogWriteResolution(12);
+   pinMode(trigPin, OUTPUT);
+   pinMode(echoPin, INPUT);
 }
 
+void echo(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  Serial.println(duration);
+  delay(2500);
+}
 void loop()
 {
   analogWrite(DAC0, 2037);
+  echo();
 }
