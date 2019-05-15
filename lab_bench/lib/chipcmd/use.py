433d08f7406d7c8e16738b93bad1272a09df371e
sachour <sachour@mit.edu>
2019-05-15 17:02:55 -0400

    stricter codes

--- lab_bench/lib/chipcmd/use.py
+++ lab_bench/lib/chipcmd/use.py
@@ -526,7 +526,7 @@ class UseIntegCmd(UseCommand):
     @property
     def max_error(self):
         if self._out_range == RangeType.HIGH:
-            return 0.1
+            return 0.01
         elif self._out_range == RangeType.MED:
             return 0.01
         elif self._out_range == RangeType.LOW:
@@ -659,7 +659,7 @@ class UseMultCmd(UseCommand):
     @property
     def max_error(self):
         if self._out_range == RangeType.HIGH:
-            return 0.02
+            return 0.01
         elif self._out_range == RangeType.MED:
             return 0.01
         elif self._out_range == RangeType.LOW:
