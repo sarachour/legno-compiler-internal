bc76ff053d727a19360e4d04f337f89b77d7ff98
Jessica Ray <jray@csail.mit.edu>
2019-09-11 21:01:23 -0400

    updated database

--- progs/debug/debug-cos-vga.py
+++ progs/debug/debug-cos-vga.py
@@ -2,7 +2,7 @@ from dslang.dsprog import DSProg
 from dslang.dssim import DSSim
 
 def dsname():
-  return "relcos2"
+  return "debug-cos-vga"
 
 def dsprog(prob):
   params = {
@@ -10,9 +10,9 @@ def dsprog(prob):
     'V0' :0.0,
     'one':0.9999
   }
-  prob.decl_stvar("V","{one}*(-P)","{V0}",params)
+  prob.decl_stvar("V","(-P)","{V0}",params)
   prob.decl_stvar("P","V","{P0}",params)
-  prob.emit("P","Position",params)
+  prob.emit("{one}*P","Position",params)
   prob.interval("P",-1.0,1.0)
   prob.interval("V",-1.0,1.0)
   prob.check()
