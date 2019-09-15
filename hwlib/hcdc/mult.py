dd3c7cef224d02591811041e0d3d4e5192866883
sachour <sachour@mit.edu>
2019-09-15 11:07:41 -0400

    added multiplier

--- hwlib/hcdc/mult.py
+++ hwlib/hcdc/mult.py
@@ -74,8 +74,7 @@ def is_extended_vga(mode):
   not_low = (i != enums.RangeType.LOW) and \
           (o != enums.RangeType.LOW)
   disabled = [
-    (enums.RangeType.MED,enums.RangeType.HIGH),
-    (enums.RangeType.HIGH,enums.RangeType.HIGH)
+    (enums.RangeType.MED,enums.RangeType.HIGH)
   ]
   for ci,co in disabled:
     if i == ci and o == co:
