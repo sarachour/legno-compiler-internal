539ff469be90979a898c086426e9128f0dd1c1df
Your Name <you@example.com>
2019-12-04 21:17:59 +0000

    added new visualization

--- scripts/visualize_experiments.py
+++ scripts/visualize_experiments.py
@@ -1,5 +1,7 @@
 import scripts.visualize.paper_quality_energy_runtime  \
   as paper_quality_energy_runtime
+import scripts.visualize.paper_energy_runtime  \
+  as paper_energy_runtime
 import scripts.visualize.paper_bmark_summary as paper_bmark_summary
 import scripts.visualize.paper_circuit_summary as paper_circuit_summary
 import scripts.visualize.paper_chip_summary as paper_chip_summary
@@ -15,6 +17,8 @@ from scripts.expdriver_db import ExpDriverDB
 def execute(args):
   name = args.type
   opts = {
+    'paper-energy-runtime': \
+    paper_energy_runtime.visualize,
     'paper-quality-energy-runtime': \
     paper_quality_energy_runtime.visualize,
     'paper-chip-summary': paper_chip_summary.visualize,
@@ -30,6 +34,7 @@ def execute(args):
       db = ExpDriverDB()
       opts[name](db)
     except Exception as e:
+      print("could not load database.")
       opts[name](None)
 
   else:
