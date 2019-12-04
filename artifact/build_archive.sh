26e844312c191d49c777533c3112ab27486387c9
sachour <sachour@mit.edu>
2019-12-04 17:33:01 -0500

    modified build archive

--- artifact/build_archive.sh
+++ artifact/build_archive.sh
@@ -1,1 +1,2 @@
-tar -cvzf archive.tar.gz build_archive.sh build_image.sh run_image.sh keys.zip run_all.py setup_exp_data.py table_gen.py state.db
+tar -cvzf mantis_artifact.tar.gz build_archive.sh build_image.sh run_image.sh keys.zip run_all.py setup_exp_data.py table_gen.py state.db 
+
